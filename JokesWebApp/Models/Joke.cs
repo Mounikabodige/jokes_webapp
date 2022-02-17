1a38ca54c0ab78167a9c00e11ff890742e8a9e3e
mounikabodige <mounikabodige1994@gmail.com>
2022-02-16 22:50:58 -0500

    Initial check-in of module JokesWebApp

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the joke identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the joke question.
+        /// </summary>
+        public string JokeQuestion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the joke answer.
+        /// </summary>
+        public string JokeAnswer { get; set; }
+    }
+}
